97142dec0996c03adfbd6b566e9ac9bc303d2aa0
Emirhan-26 <aksehirliogluemirhan@gmail.com>
2023-02-24 22:30:18 +0300

    initial commit

---
+++ src/helper/data.js
@@ -0,0 +1,256 @@
+const data = [
+    {
+      photographer: 'Lucía Montenegro',
+      src: {
+        large:
+          'https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      },
+    },
+    {
+      photographer: 'Veronika Bykovich',
+      src: {
+        large:
+          'https://images.pexels.com/photos/1707820/pexels-photo-1707820.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      },
+    },
+    {
+      photographer: 'ἐμμανυελ  ',
+      src: {
+        large:
+          'https://images.pexels.com/photos/1722183/pexels-photo-1722183.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      },
+    },
+    {
+      photographer: 'Eugene Golovesov',
+      src: {
+        large:
+          'https://images.pexels.com/photos/3052361/pexels-photo-3052361.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      },
+    },
+    {
+      photographer: 'op23',
+      src: {
+        large:
+          'https://images.pexels.com/photos/12166061/pexels-photo-12166061.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+      },
+    },
+    {
+      photographer: 'Nina  Hill',
+      src: {
+        large:
+          'https://images.pexels.com/photos/12168556/pexels-photo-12168556.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+      },
+    },
+    {
+      photographer: 'Jonathan Borba',
+      src: {
+        large:
+          'https://images.pexels.com/photos/2404843/pexels-photo-2404843.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      },
+    },
+    {
+      photographer: 'Enrique Hoyos',
+      src: {
+        large:
+          'https://images.pexels.com/photos/802024/pexels-photo-802024.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      },
+    },
+    {
+      photographer: 'Malik Skydsgaard',
+      src: {
+        large:
+          'https://images.pexels.com/photos/12132217/pexels-photo-12132217.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+      },
+    },
+    {
+      photographer: 'Fidan Nazim qizi',
+      src: {
+        large:
+          'https://images.pexels.com/photos/1538177/pexels-photo-1538177.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      },
+    },
+    {
+      photographer: 'Philip Warp',
+      src: {
+        large:
+          'https://images.pexels.com/photos/12151766/pexels-photo-12151766.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+      },
+    },
+    {
+      photographer: 'Poyee Tsang',
+      src: {
+        large:
+          'https://images.pexels.com/photos/12139617/pexels-photo-12139617.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+      },
+    },
+    {
+      photographer: 'Poyee Tsang',
+      src: {
+        large:
+          'https://images.pexels.com/photos/12139755/pexels-photo-12139755.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+      },
+    },
+    {
+      photographer: 'Blue Arauz',
+      src: {
+        large:
+          'https://images.pexels.com/photos/12151090/pexels-photo-12151090.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+      },
+    },
+    {
+      photographer: 'Eugene Golovesov',
+      src: {
+        large:
+          'https://images.pexels.com/photos/12142950/pexels-photo-12142950.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+      },
+    },
+    {
+      photographer: 'Martino Grua',
+      src: {
+        large:
+          'https://images.pexels.com/photos/379419/pexels-photo-379419.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      },
+    },
+    {
+      photographer: 'Malik Skydsgaard',
+      src: {
+        large:
+          'https://images.pexels.com/photos/12132204/pexels-photo-12132204.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+      },
+    },
+    {
+      photographer: 'Malik Skydsgaard',
+      src: {
+        large:
+          'https://images.pexels.com/photos/700974/pexels-photo-700974.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      },
+    },
+    {
+      photographer: 'Alyona Pastukhova',
+      src: {
+        large:
+          'https://images.pexels.com/photos/12136490/pexels-photo-12136490.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+      },
+    },
+    {
+      photographer: 'Rizky Sabriansyah',
+      src: {
+        large:
+          'https://images.pexels.com/photos/12128696/pexels-photo-12128696.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+      },
+    },
+    {
+      photographer: 'Kris Møklebust',
+      src: {
+        large:
+          'https://images.pexels.com/photos/12143040/pexels-photo-12143040.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+      },
+    },
+    {
+      photographer: 'op23',
+      src: {
+        large:
+          'https://images.pexels.com/photos/12125084/pexels-photo-12125084.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+      },
+    },
+    {
+      photographer: '7inchs',
+      src: {
+        large:
+          'https://images.pexels.com/photos/12092806/pexels-photo-12092806.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+      },
+    },
+    {
+      photographer: 'Taylor Marx',
+      src: {
+        large:
+          'https://images.pexels.com/photos/12089012/pexels-photo-12089012.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+      },
+    },
+    {
+      photographer: 'Jair Hernandez',
+      src: {
+        large:
+          'https://images.pexels.com/photos/12087895/pexels-photo-12087895.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+      },
+    },
+    {
+      photographer: 'Elena Rubtsova',
+      src: {
+        large:
+          'https://images.pexels.com/photos/12082485/pexels-photo-12082485.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+      },
+    },
+    {
+      photographer: 'Elena Rubtsova',
+      src: {
+        large:
+          'https://images.pexels.com/photos/12082493/pexels-photo-12082493.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+      },
+    },
+    {
+      photographer: 'GEORGE KASCHEEV',
+      src: {
+        large:
+          'https://images.pexels.com/photos/12052041/pexels-photo-12052041.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+      },
+    },
+    {
+      photographer: 'Trinity LaKose',
+      src: {
+        large:
+          'https://images.pexels.com/photos/771881/pexels-photo-771881.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      },
+    },
+    {
+      photographer: 'Omar Zetina',
+      src: {
+        large:
+          'https://images.pexels.com/photos/164583/pexels-photo-164583.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      },
+    },
+    {
+      photographer: 'Withsonya_',
+      src: {
+        large:
+          'https://images.pexels.com/photos/1034662/pexels-photo-1034662.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      },
+    },
+    {
+      photographer: 'Leah Kelley',
+      src: {
+        large:
+          'https://images.pexels.com/photos/3849168/pexels-photo-3849168.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      },
+    },
+    {
+      photographer: 'Leah Kelley',
+      src: {
+        large:
+          'https://images.pexels.com/photos/12128383/pexels-photo-12128383.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+      },
+    },
+    {
+      photographer: 'Usman ARJ',
+      src: {
+        large:
+          'https://images.pexels.com/photos/1829189/pexels-photo-1829189.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      },
+    },
+    {
+      photographer: 'Iury Bessa',
+      src: {
+        large:
+          'https://images.pexels.com/photos/3467149/pexels-photo-3467149.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      },
+    },
+    {
+      photographer: 'Alexandar Pasaric',
+      src: {
+        large:
+          'https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      },
+    },
+  ];
+  
+  export default data;
